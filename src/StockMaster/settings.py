4a2b405b533291d997c78312fa7506c9a7ea2b56
YoannMis <ymisericordia@gmx.com>
2025-05-10 17:13:02 +0200

    Update settings.py
    INSTALLED_APPS: add [’users.apps.UsersConfig’, ‘stocks.apps.StocksConfig'].

--- src/StockMaster/settings.py
+++ src/StockMaster/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'stocks.apps.StocksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -102,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
