a6472a1fa0af37f2bfaff031da140e72e0a02393
YoannMis <ymisericordia@gmx.com>
2025-05-20 22:31:51 +0200

    Update UserProfile model

--- src/users/models.py
+++ src/users/models.py
@@ -1,11 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.db.models import TextChoices
+
 from stocks.models import Warehouse
 
+# Get the currently active user model
+user = get_user_model()
 
+
+# User profile model
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    warehouses = models.ManyToManyField(Warehouse, related_name='user_profiles')
+    # Different user profiles in the company
+    class Profiles(TextChoices):
+        ADMIN = "ADM", "Admin"
+        MANAGER = "MAN", "Manager"
+        OPERATOR = "OPE", "Operator"
+        USER = "USR", "User"
 
-    # def __str__(self):
-    #     return self.user.name
+    # Link to the User model
+    user = models.OneToOneField(user, on_delete=models.CASCADE, related_name="user")
+    # Link to the Warehouse model: where the user can access
+    warehouses = models.ManyToManyField(Warehouse, related_name="warehouses")
+    # User's profile, a simple user by default
+    profile = models.CharField(max_length=10, default=Profiles.USER, choices=Profiles, verbose_name="user profile")
+
+    def __str__(self):
+        return f"{self.user} ({self.profile})"
