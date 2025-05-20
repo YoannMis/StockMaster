fc2e96e0c6b1e8ca9e1c21b3dc6d422ba693462b
YoannMis <ymisericordia@gmx.com>
2025-05-20 22:24:45 +0200

    Register new models

--- src/stocks/admin.py
+++ src/stocks/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from stocks.models import Warehouse
+from stocks.models import Warehouse, Product, Stock, StockMovement
 
 admin.site.register(Warehouse)
+admin.site.register(Product)
+admin.site.register(Stock)
+admin.site.register(StockMovement)
