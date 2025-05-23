284cf3c90632c90f1fc7f0f7214a5368c97f8716
YoannMis <ymisericordia@gmx.com>
2025-05-23 22:37:45 +0200

    Update settings
    Hide SECRET_KEY with Django-environ.

--- src/StockMaster/settings.py
+++ src/StockMaster/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
+env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)@e51ouc3bd02v3us=dceb598iot$b*ajr6=oevwrv5bzoy2_p'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
