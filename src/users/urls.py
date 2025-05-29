4c440faa1c3ea4357299703656bf8d0851b7ff4f
YoannMis <ymisericordia@gmx.com>
2025-05-29 16:18:42 +0200

    Add logout functionnality
    /users/index.html: add logout form tag with a logout submit button.
    
    /users/urls.py: add logout url pattern with Django Built-in LogoutView as view.

--- src/users/urls.py
+++ src/users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 
-app_name = 'users'
+app_name = "users"
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login-page"),
+    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login-page"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="users/login.html"), name="logout-page"),
 ]
